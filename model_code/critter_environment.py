0b2a52fbb9ea2091d038fb36afad226eceeda994
JustinHarder <justin.tienken-har13@ncf.edu>
2020-11-15 23:36:45 -0500

    Updated environment, and wrote a simple unit test. Returns what we want.

--- model_code/critter_environment.py
+++ model_code/critter_environment.py
@@ -4,25 +4,23 @@ import numpy as np
 
 class Environment:
     def __init__(self, DATA_FILE_NAME, EPSILON_PERTURBATIONS = False):
-        self.trajectory, self.data = data_initializer(DATA_FILE_NAME) #TODO: Needs some modifications inn trajeectory Processor
-        self.index_lookup = dict((tuple(y),x) for x,y in self.data.items())
+        self.epsilon_pert = EPSILON_PERTURBATIONS
+        self.data, self.index_lookup, self.subsequent, self.trajectory = data_initializer(DATA_FILE_NAME)
         if len(self.index_lookup) != len(self.data):
             print("Warning there are some conflicts in the data lookup")
             print("Attempting to correct this problem")
-            self.index_lookup = dict()
+            raise("Just kidding, please edit the environment to fix this problem. I've removed it due to our data having nice qualities. Turns out I was wrong.")
             for x, y in self.data.items():
                 y = tuple(y)
                 if y in self.index_lookup:
                     if type(self.index_lookup[y]) is int:
                         self.index_lookup[y] = [self.index_lookup[y]]
-                    index_lookup[y].append(x)
+                    self.index_lookup[y].append(x)
                 else:
                     self.index_lookup[y] = x
 
     def make_start_state(self):
-        """
-        TODO: Look at how trajectory processor handles this. Important that if we go with the subsequent dictionary, then we'll be grabbing actually relvant frames.
-        """
+        """Sets implicit start_index and final_index for some number of trajectories"""
         self.start_index, self.final_index = self.trajectory()
         return self.data[self.start_index]
 
@@ -31,24 +29,30 @@ class Environment:
         Should take the current state and the action and return the new state and the reward.
         """
         index = self.index_lookup[tuple(current_state)]
-        if type(index) is list:
-            for i in index:
-                if i < self.final_index and i > self.start_index:
-                    index = i
-                    break
-        next_index = self.next_index_lookup.setdefault(index, None)
-        if (next_index) == floor(self.data["end"]) or next_index == None: #TODO subsequent element index
-            if next_index == None:
-                raise("Reached end, end, end of trajectory without escaping first!")
-            else:
-                returned_state = None
-                next_state = self.data[next_index]
+        next_index = self.subsequent[index]
+        next_state = self.data[next_index]
+        if next_index == self.final_index:
+            returned_state = None
         else:
-            next_state = self.data[next_index]
             returned_state = next_state
         #now calculate reward
-        if EPSILON_PERTURBATIONS:
+        if self.epsilon_pert:
             guess = current_state + picked_action
         else:
             guess = picked_action
+        reward = -np.linalg.norm(guess - next_state)
         return returned_state, reward
+
+if __name__ == "__main__":
+    history = []
+    env = Environment(DATA_FILE_NAME, True)
+    for i in range(1000):
+        start = env.make_start_state()
+        for j in range(5):
+            current_state = start
+            while current_state is not None:
+                x = np.zeros(20)
+                current_state, reward = env.state_and_reward(current_state,x)
+                history.append(reward)
+
+    print(history)
