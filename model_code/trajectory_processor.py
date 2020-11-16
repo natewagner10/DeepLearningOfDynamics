0bb373b77e56d74aa680a171b1cf0428b2867a59
JustinHarder <justin.tienken-har13@ncf.edu>
2020-11-15 23:36:11 -0500

    Preprocess the data, and some cursed code

--- model_code/trajectory_processor.py
+++ model_code/trajectory_processor.py
@@ -13,37 +13,66 @@ ProjectDirectory:
 import os
 import random
 #from pandas import read_csv
-'''
-def trajectory_grabber_initializer(absolute_path_to_trajectory_folder: str, file_prefix: str, file_suffix: str, frame_skip: int, frame_num_col: str = "frame_num") -> function:
+"""
+kinda_cursed is definitely cursed - no question about it - but it provides considerable speed benefits, and completely generalizes situations where there isn't a single long path through a video (multiple clips, for example).
+We encode the connections between frames as a dictionary, and the function allows you to find n frames away from x (returning the last frame m < n if there are not n more frames after x).
+If x is not a valid frame (or there is no subsequent, return None).
+
+Used as:
+
+kinda_cursed(n)(x) = nth frame after x
+
+Just a bit of exposition on the subesequent code:
+
+(lambda func: lambda x: lambda y: func(x,y) )
+This takes any function, f, that takes two arguments and curries it, i.e., returns a function g, such that f(x, y) = g(x)(y)
+
+(lambda r: lambda n, x: r(r, n, x, n))
+This takes a (carefully crafted) function whose first argument is implicitly a reference to itself, and returns a version of only two arguments that makes the first argument a reference to itself. Essentially allowing for completely anonymous recursive functions (defining functions has significant overhead). This presumes the passed in function is using the first argument as self reference.
+
+lambda s, n, x, no:
+  ssframe[x] if n == 1 and x in ssframe else(
+    s(s, n-1, ssframe[x], n) if x in ssframe else (
+        x if n != no else None
+    )
+  )
+Carefully crafted function that performs ssframe[x] (subsequent frame of x), n number of times. If starting x is not a valid frame, returns none. If some mth frame has no subsequent frame, returns that frame.
+"""
+
+def data_initializer(file_name, trajectory_length = 30, midpoints = 2):
     """
-    Takes absolute path to trajectory folder, file prefixes in the directory, file suffix and the number of frames that are skipped across all trajectories.
-
-    Returns the formatted data grabber that doesn't have to preprocess anything.
+    Preprocess the data and return a generating function that'll be utilized by the environment.
     """
-    x = len(os.listdir(absolute_path_to_trajectory_folder))
-    random_csv_order = random.sample(range(x), x)
-    def formatted_data_grabber(trajectory_directory_order = random_csv_order, traj_order = random_csv_order.copy(), frame_skip = frame_skip):
-        if trajectory_directory_order == []:
-            new_epoch = random.sample(traj_order, x)
-            trajectory_directory_order.extend(new_epoch)
-        #get the trajectory
-        trajectory_number = trajectory_directory_order.pop()
-        #load that trajectory into pandas dataframe
-        file_name = file_prefix+ str(trajectory_number) + file_suffix
-        file_name = os.path.join(absolute_path_to_trajectory_folder, file_name)
-        df = read_csv(file_name)
-        # get the first frame number
-        first = df[0,1]
-        #convert dataframe as dicitonary of
-        data_as_dict = df.set_index(frame_num_col).T.to_dict('list').itmes()
-        output_dictionary = dict((x,np.array(y)) for x,y in data_as_dict )
-        return None
-    return formatted_data_grabber
+    import pickle
+    data = pickle.load(open(file_name, "rb"))
+    #Make the subsequent frame dictionary
+    keys = list(data.keys())
+    keys.sort()
+    lookup = dict((tuple(y),x) for x,y in data.items())
+    subsequent = dict()
+    for i in range(len(keys)):
+        if i != 0:
+            key = keys[i-1]
+            value = keys[i]
+            subsequent[key] = value
+    kinda_cursed = (lambda func: lambda x: lambda y: func(x,y) )(
+  (lambda r: lambda n, x: r(r, n, x, n))(
+    lambda s, n, x, no: subsequent[x] if n == 1 and x in subsequent else( s(s, n-1, subsequent[x], n) if x in subsequent else (x if n != no else None) ) )
+    )(trajectory_length)
+    valid_start = keys[::int(round(trajectory_length/midpoints))]
+    random_start = list(random.sample(valid_start, len(valid_start)))
+    def training_generator(starts = random_start, backup = valid_start):
+        if starts == []:
+            starts.extend(random.sample(backup, len(backup)))
+        start_frame = starts.pop()
+        end_frame = kinda_cursed(start)
+        return start_frame, end_frame
+    return data, lookup, subsequent, training_generator
 
-print(type(trajectory_grabber_initializer("poo", "zoo", "zoo", 5)))
+
+
+
 '''
-
-
 def data_initializer(file_name, trajectory_length = 30):
     """
     Preprrocesses some data and returns a generating function that spits out start and end frame numbers.
@@ -67,7 +96,7 @@ def data_initializer(file_name, trajectory_length = 30):
         data['start'], data['end'] = first_frame, last_frame
         return first_frame, last_frame
     return training_generator, data
-
+'''
 
 if __name__ == "__main__":
     FILE = ""
